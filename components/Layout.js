db5467b8d1076070173e3b215c768f1add4f9f4c
tomioka-k <tomioka-k@tajima.co.jp>
2021-10-10 18:50:32 +0900

    blog page getStaticProps

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children, title = "Default title" }) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <main className="flex flex-1 jusfity-center items-center w-screen flex-col">
+      <main className="flex flex-1 justify-center items-center w-screen flex-col">
         {children}
       </main>
       <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
