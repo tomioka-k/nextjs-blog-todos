f515aa5790e5e9fb87efa21f6cc10ee34a2b49ea
tomioka-k <tomioka-k@tajima.co.jp>
2021-10-10 13:26:47 +0900

    Layout component

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Layout = ({ children, title = "Default title" }) => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 jusfity-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+        @Udemy 2021
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
