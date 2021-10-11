b4b21bc33af94a6319cc63393cb5630f20894635
tomioka-k <tomioka-k@tajima.co.jp>
2021-10-11 14:17:10 +0900

    task detail page

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,9 +1,33 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Task from "../components/Task";
+import { getAllTasksData } from "../lib/tasks";
+import useSWR from "swr";
+import { useEffect } from "react";
 
-export default function TaskPage() {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticfilterdTasks }) {
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticfilterdTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
     <Layout title="task page">
+      <ul>
+        {filteredTasks?.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </ul>
+
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -26,4 +50,12 @@ export default function TaskPage() {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const staticfilterdTasks = await getAllTasksData();
+  return {
+    props: { staticfilterdTasks },
+    revalidate: 3,
+  };
 }
